245d802a2119fa8152275c241cb3921234db8ad5
alligator-io <markus@sonium.org>
2015-04-05 19:42:10 +0200

    Added connect

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var _ = require('icebreaker')
 var EventEmitter = require('events').EventEmitter
+
 function isFunction(obj) {
   return typeof obj === 'function'
 }
+
 _.mixin({
   agent: function (params) {
     var a = function (options) {
@@ -11,8 +13,8 @@ _.mixin({
       var apply = function (options) {
         if (typeof options === 'object') {
           for (var k in options) {
-            var value = options[k]
-            if (!isFunction(value)) emitter[k] = value
+          var value = options[k]
+          if (!isFunction(value)||k==='peers') emitter[k] = value
           }
         }
       }
@@ -35,12 +37,71 @@ _.mixin({
         this.emit('stop')
       }
 
+      var seen = {}
+
+      emitter.connect= function () {
+        var list = function(){
+          return isFunction(this.peers)?this.peers():this.peers
+        }.bind(this)
+
+        return _(
+        _.filter(function (d) {
+          if (!d) return false
+          for (var i in seen) {
+            var p = seen[i]
+            if (p.port === d.port && p.address === d.address) {
+              return false
+            }
+          }
+          return true
+        }),
+        _.sink(function (read) {
+          read(null, function next(end, d) {
+            if (end) return
+
+            _(
+            list(),
+            _.find(function (p) {
+              return p && p.port === d.port && p.address === d.address
+            },
+            function (err, peer) {
+              if (err || peer) return read(null, next)
+              _(
+                list(),
+                _.find(function (peer) {
+                    return peer.name === d.name
+                  },
+                  function (err, peer) {
+
+                    if (!peer) return read(null, next)
+                    _(
+                      _.values(peer.connections),
+                      _.find(function (connection) {
+                          if (typeof d.port === 'string') {
+                            return connection.port === d.port
+                          }
+                          return connection.address === d.address
+                        },
+                        function (err, found) {
+                          if (err) return read(null, next)
+                          if (!found) peer.connect(d)
+                          read(null, next)
+                        }
+                      )
+                    )
+                  })
+              )
+
+            })
+            )
+          })
+
+        })())
+      }.bind(emitter)
+
+
       apply(params)
       apply(options)
-
-      for (var key in params) {
-        if (!isFunction(params[key])) this[key] = params[key]
-      }
 
       var handle = function (key) {
         if (isFunction(params[key])) {
@@ -61,8 +122,4 @@ _.mixin({
 
     return a
   }
-})
-
-if (!_.agents) _.mixin({
-  agents: {}
 })
